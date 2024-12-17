d414774a8deccbdca2bbc5aa4db88fc37894d362
AlbertoLucass <trindade_neto@hotmail.com>
2024-12-17 17:57:45 -0300

    feat: stryle sidebar

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,12 +1,18 @@
-import { Header } from "./components/Header"
-import './global.css'
-
+import { Header } from "./components/Header";
+import styles from "./App.module.css";
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
-
   return (
-   <Header/>
-  )
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main></main>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
